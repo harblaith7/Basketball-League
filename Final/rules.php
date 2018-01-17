e6e44636545586b260831f55b1c511d5841f0ac3
harblaith7 <35265876+harblaith7@users.noreply.github.com>
2018-01-16 19:26:42 -0500

    fixed template and made rules page

--- Final/rules.php
+++ Final/rules.php
@@ -10,13 +10,19 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-12 col-lg-12 element-content">
-                            
-                            
-                            
-                            
-                            
-                            
-                             </div>
+        
+                            <!-- Content -->
+        
+        		<div class="heading heading-v1 text-center mb-3 mt-5 bg-white bg-gray">
+			<h2 class="heading-v1-title">Lorem ipsum.</h2>
+		</div>
+
+		<p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde officia dolores sint iusto sit temporibus nam, neque quidem aliquam perspiciatis illo ipsum, sequi aperiam! Voluptas magni ipsam officia nesciunt quos?</p>
+		<p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non, fugiat, eos. Numquam sit molestias, impedit unde consectetur animi, quas neque, vitae necessitatibus nam voluptate aspernatur repellat doloribus a similique. Minus.</p>
+        
+        
+        <!-- FOOTER -->
+         </div>
                     </div>
                     <!-- element-content ends -->
                 </div>
--- Final/template.php
+++ Final/template.php
@@ -1,17 +1,33 @@
 <html lang="en">
 <?php include "head.php"; ?>
+
 <body id="body" class="home-classic">
     <!-- HEADER -->
     <?php include "nav.php"; ?>
-    <div class="main-wrapper" style="margin-bottom: 20px;">
+    <div class="main-wrapper">
+        <div class="main-wrapper">
+            <div class="grid-wrapper section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-lg-12 element-content">
         <!--        
                   CONTENT
                                 -->
     
+        <!-- FOOTER -->
+         </div>
+                    </div>
+                    <!-- element-content ends -->
+                </div>
+            </div>
+            <!-- element main container ends -->
+        </div>
+
         <!-- FOOTER -->
         <?php include "foot-nav.php"; ?>
     </div>
     <!-- JAVASCRIPTS -->
     <?php include "js-compile.php"; ?>
 </body>
+
 </html>
