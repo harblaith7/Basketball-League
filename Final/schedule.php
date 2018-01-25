37bf930b7444757f39c14c0e496c7bce6a4edff7
abdu997 <aamou094@uottawa.ca>
2018-01-24 21:12:44 -0500

    Finished first draft of website

---
+++ Final/schedule.php
@@ -0,0 +1,92 @@
+<html lang="en">
+
+<head>
+    <?php include "head.php"?>
+</head>
+
+<body id="body" class="b-element">
+    <!-- Preloader -->
+    <div id="preloader" class="smooth-loader-wrapper">
+        <div class="smooth-loader">
+            <div class="loader1">
+                <div class="loader-target">
+                    <div class="loader-target-main"></div>
+                    <div class="loader-target-inner"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "nav.php";?>
+    <div class="main-wrapper">
+        <section class="">
+            <div class="bg-image-holder bredcrumb bg-primary">
+                <!-- style="background-image: url('img/promo-1.jpg');" -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h2>Schedule </h2>
+                            <hr class="hr_narrow  hr_color">
+                            <p>Players are advised to be present 20 minutes before their game</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- container ends -->
+            </div>
+        </section>
+        <section class="bg-sand hero-block home-about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12" style="color: black">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Game #</th>
+                                    <th>Teams</th>
+                                    <th>Date</th>
+                                    <th>Start Time</th>
+                                    <th>End Time</th>
+                                    <th>Location</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>###</td>
+                                    <td>###</td>
+                                    <td>###</td>
+                                    <td>###</td>
+                                    <td>###</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <!-- table-inverse -->
+                        <table class="table table-inverse">
+
+                        </table>
+
+                        <!-- table-striped -->
+                        <table class="table table-striped">
+
+                        </table>
+
+                        <!-- table-bordered -->
+                        <table class="table table-bordered">
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- FOOTER -->
+        <?php include "foot-nav.php";?>
+    </div>
+
+
+    <!-- JAVASCRIPTS -->
+    <?php include "js-compile.php";?>
+
+</body>
+
+</html>
--- Final/teams.php
+++ Final/teams.php
@@ -1,69 +1,107 @@
 <html lang="en">
-<?php include "head.php"; ?>
 
-<body id="body" class="home-classic">
-    <!-- HEADER -->
-    <?php include "nav.php"; ?>
-    <div class="main-wrapper">
-        <div class="main-wrapper">
-            <div class="grid-wrapper section">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-lg-12 element-content">
-                            <!-- Table -->
-                            <div class="display-single_element" style="margin-top:100px;margin-bottom:100px">
-                                <p style="font-size: 18px">Standings</p>
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>Rank</th>
-                                            <th>Team</th>
-                                            <th>Wins</th>
-                                            <th>Losses</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Jaguars</td>
-                                            <td>10</td>
-                                            <td>2</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Mud Killers</td>
-                                            <td>7</td>
-                                            <td>5</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Larry Birdz</td>
-                                            <td>3</td>
-                                            <td>12</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Not in my House</td>
-                                            <td>0</td>
-                                            <td>15</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- Defining states end -->
-                        </div>
-                    </div>
-                    <!-- element-content ends -->
+<head>
+    <?php include "head.php"?>
+</head>
+
+<body id="body" class="b-element">
+    <!-- Preloader -->
+    <div id="preloader" class="smooth-loader-wrapper">
+        <div class="smooth-loader">
+            <div class="loader1">
+                <div class="loader-target">
+                    <div class="loader-target-main"></div>
+                    <div class="loader-target-inner"></div>
                 </div>
             </div>
-            <!-- element main container ends -->
+        </div>
+    </div>
+    <?php include "nav.php";?>
+    <div class="main-wrapper">
+        <section class="">
+            <div class="bg-image-holder bredcrumb bg-primary">
+                <!-- style="background-image: url('img/promo-1.jpg');" -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h2>Teams </h2>
+                            <hr class="hr_narrow  hr_color">
+                        </div>
+                    </div>
+                </div>
+                <!-- container ends -->
+            </div>
+        </section>
+        <section class="home-blog bg-sand">
+            <div class="container">
+                <!-- section title ends -->
+                <div class="row ">
+                    <div class="col-md-6">
+                        <div class="media blog-media" data-toggle="modal" data-target="#exampleModalLong2">
+                            <img class="d-flex" src="img/blog/b1.jpg" alt="Generic placeholder image">
+                            <div class="circle">
+                                <h5 class="day">14</h5>
+                                <span class="month">sep</span>
+                            </div>
+                            <div class="media-body">
+                                <a href="">
+                                    <h5 class="mt-0">Standard Blog Post</h5>
+                                </a> Sodales aliquid, in eget ac cupidatat velit autem numquam ullam ducimus occaecati placeat error.
+                                <a href="blog-post-left-sidebar.html" class="post-link">Read More</a>
+                                <ul>
+                                    <li>by: Admin</li>
+                                    <li class="text-right"><a href="blog-post-left-sidebar.html">07 comments</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Modal -->
+        <div class="modal fade " id="exampleModalLong2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="false">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <button type="button" class="close push-xs-right" data-dismiss="modal" aria-label="Close">
+							<i class="fa fa-close"></i>
+						</button>
+                        <div class="container-fluid modal-item">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="single_item-img">
+                                        <img src="img/modal1.jpg" alt="image">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="single_item-details">
+                                        <h2>Modal Title</h2>
+
+                                        <p class="pv30">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio assumenda numquam dolor illo praesentium.
+                                        </p>
+                                        <ul class="list-group no-border">
+                                            <li class="list-group-item"><a href="#"><i class="fa fa-dot-circle-o"></i> icon List item without border</a></li>
+                                            <li class="list-group-item"><a href="#"><i class="fa fa-dot-circle-o"></i> icon List item without border</a></li>
+                                            <li class="list-group-item"><a href="#"><i class="fa fa-dot-circle-o"></i> icon List item without border</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- FOOTER -->
-        <?php include "foot-nav.php"; ?>
+        <?php include "foot-nav.php";?>
     </div>
+
+
     <!-- JAVASCRIPTS -->
-    <?php include "js-compile.php"; ?>
+    <?php include "js-compile.php";?>
+
 </body>
 
 </html>
