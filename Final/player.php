31344994d1f003c3cafa2d7e6095e0da406f67a1
abdu997 <aamou094@uottawa.ca>
2018-02-05 18:19:17 -0500

    Adjusted template

---
+++ Final/player.php
@@ -0,0 +1,132 @@
+<html lang="en">
+
+<head>
+	<?php include "head.php"?>
+</head>
+
+<body id="body" class="b-element">
+	<!-- Preloader -->
+	<div id="preloader" class="smooth-loader-wrapper">
+		<div class="smooth-loader">
+			<div class="loader1">
+				<div class="loader-target">
+					<div class="loader-target-main"></div>
+					<div class="loader-target-inner"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include "nav.php";?>
+		<div class="main-wrapper">
+			<section class="">
+				<div class="bg-image-holder bredcrumb bg-primary">
+					<!-- style="background-image: url('img/promo-1.jpg');" -->
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8">
+								<h2>TITLE </h2>
+								<hr class="hr_narrow  hr_color">
+								<p>DESCRIPTION</p>
+							</div>
+						</div>
+					</div>
+					<!-- container ends -->
+				</div>
+			</section>
+			<!-- Member details -->
+			<section class="member-details">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 col-md-4">
+							<div class="img-container"> <img src="img/about/team4.jpg" alt="team member" class="img-full"> </div>
+						</div>
+						<div class="col-lg-9 col-md-8">
+							<div class="member_designation">
+								<h2>Bodrum Salvador</h2> <span>Co Founder</span> </div>
+							<div class="member_desc">
+								<p> Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt.ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. </p>
+								<ul class="styled_list">
+									<li class=""><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Morbi fermentum felis nec</li>
+									<li class=""><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Morbi fermentum felis nec gravida tempus.</li>
+									<li class=""><i class="fa fa-chevron-circle-right" aria-hidden="true"></i> Morbi fermentum felis nec gravida</li>
+								</ul>
+							</div>
+							<div class="bg-image " style="background-image: url('img/slider/sl1.jpg');">
+								<div class="member_contact">
+									<div class="row">
+										<div class="col-lg-4  mb-3 mb-lg-0">
+											<div class="media-box">
+												<div class="media-icon"> <i class="fa fa-tablet" aria-hidden="true"></i> </div>
+												<div class="media-content">
+													<h5>Phone</h5>
+													<p><a href="callto">(+1) 251-235-3256</a></p>
+												</div>
+											</div>
+										</div>
+										<div class="col-lg-4  mb-3 mb-lg-0">
+											<div class="media-box">
+												<div class="media-icon"> <i class="fa fa-envelope-o" aria-hidden="true"></i> </div>
+												<div class="media-content">
+													<h5>Email</h5>
+													<p><a href="mailto">info@example.com</a></p>
+												</div>
+											</div>
+										</div>
+										<div class="col-lg-4">
+											<div class="social-icons">
+												<button class="btn btn-social outlined"><i class="fa fa-facebook-f"></i></button>
+												<button class="btn btn-social outlined"><i class="fa fa-twitter"></i></button>
+												<button class="btn btn-social outlined"><i class="fa fa-linkedin"></i></button>
+												<button class="btn btn-social outlined"><i class="fa fa-pinterest-p"></i></button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="member_desc">
+								<h4>Persional Information</h4>
+								<p> Vinteger eu libero rutrum, imperdiet arcueniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit volup tatem. accusantium doloremque laudantium. </p>
+								<h4>Skills</h4>
+								<!-- progressbar starts -->
+								<div class="progress-holder">
+									<div class="barWrapper"> <span class="progressText"><B>Javascript</B></span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div> <span class="popOver" data-toggle="tooltip" data-placement="top" title="80%"> </span> </div>
+									</div>
+									<div class="barWrapper"> <span class="progressText"><B>Photoshop</B></span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div> <span class="popOver" data-toggle="tooltip" data-placement="top" title="95%"> </span> </div>
+									</div>
+									<div class="barWrapper"> <span class="progressText"><B>Web Design</B></span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div> <span class="popOver" data-toggle="tooltip" data-placement="top" title="85%"> </span> </div>
+									</div>
+									<div class="barWrapper"> <span class="progressText"><B>Wordpress</B></span>
+										<div class="progress">
+											<div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div> <span class="popOver" data-toggle="tooltip" data-placement="top" title="75%"> </span> </div>
+									</div>
+								</div>
+								<!-- progressbar ends -->
+							</div>
+							<div class="row ">
+								<div class="col-lg-6 member_desc">
+									<h4>Nec nisl iaculis pulv</h4>
+									<p> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium. </p>
+								</div>
+								<div class="col-lg-6 member_desc">
+									<h4>Aesent nec nisl</h4>
+									<p> Cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium </p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- FOOTER -->
+			<?php include "foot-nav.php";?>
+		</div>
+		<!-- JAVASCRIPTS -->
+		<?php include "js-compile.php";?>
+</body>
+
+</html>
--- Final/template.php
+++ Final/template.php
@@ -1,31 +1,52 @@
 <html lang="en">
-<?php include "head.php"; ?>
 
-<body id="body" class="home-classic" ng-app="leagueApp" ng-controller="leagueController">
-    <!-- HEADER -->
-    <?php include "nav.php"; ?>
-    <div class="main-wrapper">
-        <div class="main-wrapper">
-            <div class="grid-wrapper section">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-lg-12 element-content">
-							<!--        
-									  CONTENT
-													-->
-						</div>
-                    </div>
-                    <!-- element-content ends -->
+<head>
+    <?php include "head.php"?>
+</head>
+
+<body id="body" class="b-element">
+    <!-- Preloader -->
+    <div id="preloader" class="smooth-loader-wrapper">
+        <div class="smooth-loader">
+            <div class="loader1">
+                <div class="loader-target">
+                    <div class="loader-target-main"></div>
+                    <div class="loader-target-inner"></div>
                 </div>
             </div>
-            <!-- element main container ends -->
         </div>
+    </div>
+    <?php include "nav.php";?>
+    <div class="main-wrapper">
+        <section class="">
+            <div class="bg-image-holder bredcrumb bg-primary">
+                <!-- style="background-image: url('img/promo-1.jpg');" -->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h2>TITLE </h2>
+                            <hr class="hr_narrow  hr_color">
+                            <p>DESCRIPTION</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- container ends -->
+            </div>
+        </section>
+		<!--
+
+					CONTENT
+							
+									-->
 
         <!-- FOOTER -->
-        <?php include "foot-nav.php"; ?>
+        <?php include "foot-nav.php";?>
     </div>
+
+
     <!-- JAVASCRIPTS -->
-    <?php include "js-compile.php"; ?>
+    <?php include "js-compile.php";?>
+
 </body>
 
 </html>
